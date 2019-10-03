8a484996d0836d521ccf3442cc482e376cfce595
AlIbay8 <supercrazyai@gmail.com>
2019-10-03 00:32:31 +0000

    added pig function: returns all of word except first character, added latin function.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
 $( "document" ).ready(function() {
+    $("#button").click(function() {
+        var string = $(".input").val().toLowerCase();
 
+        var pig = pig(string);
+        var latin = latin(string);
 
-
+        $(".output").append("<p>" + pig + latin + "</p>");
+    })
 });
+
+function pig(word) {
+    var array = word.split("");
+    var slice = array.slice(1);
+    return slice.join("");
+}
+
+function latin(word) {
+    return word.charAt(0) + "ay";
+}
