784a849da6df8c41fe46e749c99b77cb32a20d71
AlIbay8 <supercrazyai@gmail.com>
2019-10-10 00:07:34 +0000

    working pig latinizer

--- js/script.js
+++ js/script.js
@@ -1,20 +1,35 @@
+var vowels = ["a", "e", "i", "o", "u"];
+
 $( "document" ).ready(function() {
     $("#button").click(function() {
         var string = $(".input").val().toLowerCase();
+        var words = string.split(" ");
+        var vowel = false;
 
-        var pig = pig(string);
-        var latin = latin(string);
+        words.forEach(function(word) {
+            var pig1 = pig(word);
+            var latin1 = latin(word);
+            $(".output").append("<p>" + pig1 + latin1 + "</p>");
+        })
 
-        $(".output").append("<p>" + pig + latin + "</p>");
+        $(input).val("");
     })
 });
 
 function pig(word) {
-    var array = word.split("");
-    var slice = array.slice(1);
-    return slice.join("");
+    if (word.charAt(0) === "a" || word.charAt(0) === "e" || word.charAt(0) === "i" || word.charAt(0) === "o" || word.charAt(0) === "u") {
+        return "";
+    } else {
+        var array = word.split("");
+        var slice = array.slice(1);
+        return slice.join("");
+    }
 }
 
 function latin(word) {
-    return word.charAt(0) + "ay";
+    if (word.charAt(0) === "a" || word.charAt(0) === "e" || word.charAt(0) === "i" || word.charAt(0) === "o" || word.charAt(0) === "u") {
+        return word + "way";
+    } else {
+        return word.charAt(0) + "ay";
+    }
 }
